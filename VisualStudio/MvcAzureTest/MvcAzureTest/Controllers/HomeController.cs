40fdfd325a0a6583254b96d70c85c6e960ae8da4
victortuga <victor_fing@hotmail.com>
2016-04-18 21:01:47 -0300

    Agrego el proyecto de prueba para subir archivos al blob storage

---
+++ VisualStudio/MvcAzureTest/MvcAzureTest/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using MvcAzureTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcAzureTest.Controllers
+{
+    public partial class HomeController : Controller
+    {
+        
+        public virtual ActionResult Index()
+        {
+            ActionResult result;
+            try
+            {
+                MvcAzureTest.Models.VMStorage vmStorage = new Models.VMStorage();
+                result = View(MVC.Home.Views.ViewNames.Index, vmStorage);
+                
+            }
+            catch (Exception ex)
+            {
+                return Content(ex.ToString());
+            }
+
+            return result;
+        }
+
+        public virtual ActionResult Subir(VMStorage vmStorage)
+        {
+            ActionResult result = null;
+            try
+            {
+                vmStorage.SubirArchivo();
+                result = View(MVC.Home.Views.ViewNames.Index, vmStorage);
+            }
+            catch (Exception ex)
+            {
+                result = Content(ex.ToString());
+            }
+            return result;
+        }
+
+        public virtual ActionResult IngresarCuenta(VMStorage vmStorage)
+        {
+            ActionResult result = null;
+            try
+            {
+                vmStorage.LoadContenedores();
+                result = View(MVC.Home.Views.ViewNames.Subir, vmStorage);
+            }
+            catch (Exception ex)
+            {
+                Content(ex.ToString());
+            }
+            return result;
+        }
+
+    }
+}
---
+++ VisualStudio/MvcAzureTest/MvcAzureTest/Models/VMStorage.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcAzureTest.Models
+{
+    public class VMStorage
+    {
+        public string NombreCuenta { get; set; }
+
+        public string ClaveCuenta { get; set; }
+
+        public string Contenedor { get; set; }
+
+        public List<SelectListItem> Contenedores { get; set; }
+
+        public HttpPostedFileBase Archivo { get; set; }
+
+
+
+        internal void LoadContenedores()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=" + NombreCuenta + ";AccountKey=" + ClaveCuenta);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            IEnumerable<CloudBlobContainer> blobContainers = blobClient.ListContainers();
+            Contenedores = new List<SelectListItem>();
+            foreach(CloudBlobContainer cont in blobContainers)
+            {
+                Contenedores.Add(new SelectListItem() { Value = cont.Name, Text = cont.Name });
+            }
+        }
+
+        internal void SubirArchivo()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=" + NombreCuenta + ";AccountKey=" + ClaveCuenta);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference(Contenedor);
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(Archivo.FileName);
+            blockBlob.UploadFromStream(Archivo.InputStream);
+        }
+    }
+}
