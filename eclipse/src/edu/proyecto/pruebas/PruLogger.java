a964cd14c83cf8db377910911cab259da3239019
sangreaurinegra <sangreaurinegra@gmail.com>
2016-04-18 21:01:51 -0300

    Ordenando la raiz

---
+++ eclipse/src/edu/proyecto/pruebas/PruLogger.java
@@ -0,0 +1,16 @@
+package edu.proyecto.pruebas;
+import java.util.logging.Logger;
+
+public class PruLogger {
+
+	private final static Logger logger = Logger.getLogger(PruLogger.class.getName());
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	
+		logger.info("Inicio");
+	}
+
+}
