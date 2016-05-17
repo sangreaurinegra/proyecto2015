a9882b8a48b303ec61b7115321120b89f3e2fc50
sangreaurinegra <sangreaurinegra@gmail.com>
2016-05-16 22:16:02 -0300

    cambios probados primera version sin soporte a resources

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 image=$1
-tag="0.0.1"
+tag="0.1.0"
 
 if [ $# = 0 ]
 then
@@ -23,7 +23,8 @@ function docker_build()
 {
 	cd $1
 	echo -e "\n\nsudo docker build -t $1:$tag ."
-	/usr/bin/time -f "real  %e" sudo docker build -t $1:$tag .
+	#/usr/bin/time -f "real  %e" 
+	sudo docker build -t $1:$tag .
 	cd ..
 }
 
