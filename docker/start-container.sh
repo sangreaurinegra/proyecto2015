81f38b8f3eb1825bd97c2bcf199e2abc44933bb3
sangreaurinegra <sangreaurinegra@gmail.com>
2016-06-12 17:29:10 -0300

    Arreglo en generacion de master
    Arreglo para el starter de los containers

--- docker/start-container.sh
+++ docker/start-container.sh
@@ -13,7 +13,7 @@ fi
 # delete old master container and start new master container
 sudo docker rm -f master &> /dev/null
 echo "start master container..."
-sudo docker run -d -t --dns 127.0.0.1 -P --name master -h master.kiwenlau.com -w /root sangreaurinegra/master:0.1.0 &> /dev/null
+sudo docker run -d -t --dns 127.0.0.1 -P --name master -h master.kiwenlau.com -w /root master:0.1.0 &> /dev/null
 
 # get the IP address of master container
 FIRST_IP=$(docker inspect --format="{{.NetworkSettings.IPAddress}}" master)
@@ -24,7 +24,7 @@ while [ $i -lt $N ]
 do
 	sudo docker rm -f slave$i &> /dev/null
 	echo "start slave$i container..."
-	sudo docker run -d -t --dns 127.0.0.1 -P --name slave$i -h slave$i.kiwenlau.com -e JOIN_IP=$FIRST_IP sangreaurinegra/slave:0.1.0 &> /dev/null
+	sudo docker run -d -t --dns 127.0.0.1 -P --name slave$i -h slave$i.kiwenlau.com -e JOIN_IP=$FIRST_IP slave:0.1.0 &> /dev/null
 	((i++))
 done 
 
